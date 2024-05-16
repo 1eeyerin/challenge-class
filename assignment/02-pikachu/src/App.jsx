03b45a891f5761b94c1eee1fe6259e549268c1f7
Yerin <yerinlee@iyelin-ui-MacBookPro.local>
2024-05-16 22:37:19 +0900

    feat: 과제 02 - 피카츄 조작하기

--- assignment/02-pikachu/src/App.jsx
+++ assignment/02-pikachu/src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const SIZE = 40;
+const MAX_BLOCK = 9;
+
+const App = () => {
+  const [{ x, y }, setPosition] = useState({ x: 0, y: 0 });
+  const [{ translateY, scaleX }, setTransform] = useState({
+    translateY: "0px",
+    scaleX: 1,
+  });
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      switch (event.code) {
+        case "ArrowUp":
+          setPosition((prev) =>
+            prev.y <= 0 ? prev : { ...prev, y: prev.y - SIZE }
+          );
+          break;
+        case "ArrowDown":
+          setPosition((prev) =>
+            prev.y >= SIZE * MAX_BLOCK ? prev : { ...prev, y: prev.y + SIZE }
+          );
+          break;
+        case "ArrowLeft":
+          setPosition((prev) =>
+            prev.x <= 0 ? prev : { ...prev, x: prev.x - SIZE }
+          );
+          setTransform((prev) => ({ ...prev, scaleX: -1 }));
+          break;
+        case "ArrowRight":
+          setPosition((prev) =>
+            prev.x >= SIZE * MAX_BLOCK ? prev : { ...prev, x: prev.x + SIZE }
+          );
+          setTransform((prev) => ({ ...prev, scaleX: 1 }));
+          break;
+        case "Space":
+          setTransform((prev) => ({ ...prev, translateY: "-30px" }));
+
+          setTimeout(() => {
+            setTransform((prev) => ({ ...prev, translateY: "0px" }));
+          }, 150);
+          break;
+
+        default:
+          break;
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="playground">
+        <img
+          src="/src/assets/pikachu.png"
+          alt=""
+          className="pika"
+          style={{
+            left: `${x}px`,
+            top: `${y}px`,
+            transform: `translateY(${translateY}) scaleX(${scaleX})`,
+          }}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
