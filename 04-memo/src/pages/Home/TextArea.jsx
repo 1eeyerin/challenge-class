faa5f64f9b8faa86edb667b02a941dd1271a43e1
Yerin <yerinlee@iyelin-ui-MacBookPro.local>
2024-05-26 22:02:32 +0900

    feat: 디바운싱 처리^^ onChange에는 디바운싱을 직접 넣으면 안됩니다..!

--- 04-memo/src/pages/Home/TextArea.jsx
+++ 04-memo/src/pages/Home/TextArea.jsx
@@ -1,29 +1,48 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { updateMemoContent } from '../../redux/slices/memoSlice';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { debounce } from 'lodash';
+import useShallowEqualSelector from '../../hooks/useShallowEqualSelector';
 
 const TextArea = () => {
   const textareaRef = useRef(null);
+  const prevSelectedMemoIdRef = useRef();
   const dispatch = useDispatch();
-  const selectedMemoContent = useSelector(
-    ({ memo }) =>
-      memo.memos.find((item) => item.id === memo.selectedMemoId).content,
+  const [textAreaValue, setTextAreaValue] = useState('');
+
+  const { content, selectedMemoId } = useShallowEqualSelector(
+    ({ memo: { memos, selectedMemoId } }) => {
+      return {
+        content: memos.find((item) => item.id === selectedMemoId).content,
+        selectedMemoId,
+      };
+    },
   );
 
-  const handleChange = (event) => {
-    dispatch(updateMemoContent(event.target.value));
+  const debouncedUpdateMemoContent = debounce((value) => {
+    dispatch(updateMemoContent(value));
+  }, 100);
+
+  const handleChange = ({ target }) => {
+    setTextAreaValue(target.value);
+    debouncedUpdateMemoContent(target.value);
   };
 
   useEffect(() => {
+    if (prevSelectedMemoIdRef.current !== selectedMemoId) {
+      setTextAreaValue(content);
+      prevSelectedMemoIdRef.current = selectedMemoId;
+    }
+
     if (!textareaRef.current) return;
     textareaRef.current.focus();
-  }, [selectedMemoContent]);
+  }, [selectedMemoId, content]);
 
   return (
     <StyledTextarea
       ref={textareaRef}
-      value={selectedMemoContent || ''}
+      value={textAreaValue || ''}
       onChange={handleChange}
     />
   );
