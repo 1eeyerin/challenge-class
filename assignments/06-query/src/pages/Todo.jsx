cb4e359fc65952d4c2c51a6581f6a16f9ce783b6
Yerin <dev.yerin@gmail.com>
2024-06-03 17:47:04 +0900

    feat: useMutation 사용

--- assignments/06-query/src/pages/Todo.jsx
+++ assignments/06-query/src/pages/Todo.jsx
@@ -1,31 +1,28 @@
+import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
-import { useState } from "react";
+import { useRef } from "react";
 
 const ENDPOINT = "https://jsonplaceholder.typicode.com/todos";
 
 const Todo = () => {
-  const [todos, setTodos] = useState([]);
-  const [value, setValue] = useState("");
+  const ref = useRef(null);
 
-  const handleClickButton = async () => {
-    try {
-      const response = await axios.post(ENDPOINT, {
-        value,
-      });
-      setTodos((prev) => [...prev, response.data]);
-    } catch (error) {
-      console.error(error);
-    }
+  // mutateAsync는 비동기 처리를 위한 함수이다
+  const { mutate: addTodo, isPending } = useMutation({
+    mutationFn: (todo) => axios.post(ENDPOINT, { todo }),
+  });
+
+  const handleClickButton = () => {
+    // mutate 함수에서 바로 onSuccess 함수를 사용할 수 있다
+    addTodo(ref.current.value, { onSuccess: (result) => console.log(result) });
   };
 
   return (
     <>
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <button onClick={handleClickButton}>추가</button>
+      <input type="text" ref={ref} />
+      <button onClick={handleClickButton} disabled={isPending}>
+        추가
+      </button>
     </>
   );
 };
