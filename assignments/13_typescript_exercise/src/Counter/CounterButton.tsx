b970de14fef67d8055c8d886b4c8a63c8ea982e3
Yerin <dev.yerin@gmail.com>
2024-06-12 16:04:16 +0900

    feat: PropsWithChildren 기본 타입도 있음

--- assignments/13_typescript_exercise/src/Counter/CounterButton.tsx
+++ assignments/13_typescript_exercise/src/Counter/CounterButton.tsx
@@ -1,10 +1,8 @@
-type VeryUsefulType<T> = T & { children: React.ReactNode };
-
 // import React가 내장되어있어서 굳이 임폴트 안해줘도 됨
 const CounterButton = ({
   children,
   onClick: handleClick,
-}: VeryUsefulType<{ onClick: () => void }>) => {
+}: React.PropsWithChildren<{ onClick: () => void }>) => {
   return <button onClick={handleClick}>{children}</button>;
 };
 
