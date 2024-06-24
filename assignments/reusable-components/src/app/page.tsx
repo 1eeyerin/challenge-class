4155bb451997cfe5299df12ad84dfb28b15d381a
Yerin <dev.yerin@gmail.com>
2024-06-24 16:49:42 +0900

    feat: 버튼 컴포넌트 추가

--- assignments/reusable-components/src/app/page.tsx
+++ assignments/reusable-components/src/app/page.tsx
@@ -1,4 +1,5 @@
-import Chip from "./components/Chip";
+import Button from "@/components/Button";
+import Chip from "../components/Chip";
 
 export default function Home() {
   return (
@@ -9,6 +10,13 @@ export default function Home() {
       <Chip intent="warning" label="chip" />
       <Chip intent="info" label="chip" />
       <Chip intent="default" label="chip" />
+      <Button>Button</Button>
+      <Button intent="danger" size="sm" variant="outline">
+        Button2
+      </Button>
+      <Button intent="secondary" size="lg" variant="link">
+        Button3
+      </Button>
     </div>
   );
 }
--- assignments/reusable-components/src/components/Button/Button.tsx
+++ assignments/reusable-components/src/components/Button/Button.tsx
@@ -1,5 +1,59 @@
-const Button = () => {
-  return <button>Button</button>;
+import { PropsWithChildren } from "react";
+
+import { VariantProps, cva } from "class-variance-authority";
+
+const buttonVariants = cva(
+  "px-2.5 py-0.5 rounded-full hover:brightness-90 active:brightness-75",
+  {
+    variants: {
+      intent: {
+        primary: ["bg-blue-500", "border-blue-500"],
+        secondary: ["bg-yellow-500", "border-yellow-500"],
+        danger: ["bg-red-500", "border-red-500"],
+        link: "text-blue-500, underline",
+      },
+      size: {
+        sm: "text-[10px]",
+        md: "text-[12px]",
+        lg: "text-[14px]",
+      },
+      variant: {
+        outline: "border",
+        solid: "border-transparent",
+        link: "border-transparent",
+        contained: "border-transparent",
+      },
+    },
+    compoundVariants: [
+      { intent: "primary", variant: "contained", className: "text-white" },
+      { intent: "secondary", variant: "contained", className: "text-white" },
+      { intent: "danger", variant: "contained", className: "text-white" },
+      { intent: "link", variant: "link", className: "text-blue-500" },
+    ],
+    defaultVariants: {
+      intent: "primary",
+      size: "md",
+      variant: "contained",
+    },
+  }
+);
+
+type ButtonVariant = VariantProps<typeof buttonVariants>;
+
+type ButtonProps = {} & ButtonVariant & React.ComponentProps<"button">;
+
+const Button = ({
+  children,
+  intent,
+  size,
+  variant,
+  ...props
+}: PropsWithChildren<ButtonProps>) => {
+  return (
+    <button className={buttonVariants({ intent, size, variant })} {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
