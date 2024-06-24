7895d8bc23fff36fbf00f977f1159b7e744f9d9e
Yerin <dev.yerin@gmail.com>
2024-06-24 16:17:50 +0900

    feat: 넥스트 앱

--- assignments/my-first-next/src/app/posts/layout.tsx
+++ assignments/my-first-next/src/app/posts/layout.tsx
@@ -1,5 +1,7 @@
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>여기부터는 posts 레이아웃 입니다{children}</div>;
+"use client";
+
+const PostsLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
 };
 
-export default Layout;
+export default PostsLayout;
